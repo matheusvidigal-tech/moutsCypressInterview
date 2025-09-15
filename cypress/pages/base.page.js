98d3cc818a6a4eead7d22db0122309b59b149a14
MatheusVidigal <matheusvidigal.tech@outlook.com>
2025-09-15 16:59:06 -0300

    feat_addProduct - Inclusão do cenário de criação de produtos + ajustes

--- cypress/pages/base.page.js
+++ cypress/pages/base.page.js
@@ -3,15 +3,22 @@ class BasePage {
     constructor() {
     }
 
-    buttons = {
-        logout: '[data-testid="logout"]'
+    header = {
+        buttons: {
+            logout: '[data-testid="logout"]'
+        },
+
+        links: {
+            createProducts: '[data-testid="cadastrar-produtos"]',
+            listProducts: '[data-testid="listar-produtos"]'
+        }
     }
 
     /**
      * #### Verifica se o usuário foi redirecionado para a Home
      */
     checkLoginRedirect() {
-        cy.get(this.buttons).should('be.visible');
+        cy.get(this.header.buttons.logout).should('be.visible');
     }
 }
 
