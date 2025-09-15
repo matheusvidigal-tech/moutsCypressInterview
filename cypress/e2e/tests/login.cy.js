7db0c1ad7dd2859835684050cafce45ba39e853a
MatheusVidigal <matheusvidigal.tech@outlook.com>
2025-09-15 18:07:00 -0300

    fix_identation - Ajustes gerais de identação

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -1,7 +1,7 @@
 import loginPage from "../../pages/login.page";
 
 describe('Login page tests', () => {
- 
+
   it('Should show invalid user message when use an invalid password', () => {
     loginPage.performLogin(Cypress.env('COMMON_EMAIL'), '123', false);
     cy.get(loginPage.text.alert).contains('Email e/ou senha inválidos');
